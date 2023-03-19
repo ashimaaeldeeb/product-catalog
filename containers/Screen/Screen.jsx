e52da5f7ebc9ff9744bff597377587186fbad5be
AlShaimaa <36901237+ashimaaeldeeb@users.noreply.github.com>
2023-03-19 19:52:09 +0200

    chore: add status bar light

--- containers/Screen/Screen.jsx
+++ containers/Screen/Screen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BottomBar from "../BottomBar/BottomBar";
 import ScreenHeader from "../../components/ScreenHeader";
 import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "expo-status-bar";
 
 export default function Screen({
   title,
@@ -11,6 +12,7 @@ export default function Screen({
 }) {
   return (
     <NativeBaseProvider>
+      <StatusBar style="light" />
       <ScreenHeader
         hasBackButton={hasBackButton}
         title={title}
